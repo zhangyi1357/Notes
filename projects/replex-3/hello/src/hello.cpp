08333bd551b5b95a48386080adbea4a3824d68ad
zhangyi1357 <zhangyi_1357@sjtu.edu.cn>
2024-01-04 13:33:37 +0800

    replex version 3

---
+++ projects/replex-3/hello/src/hello.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+extern "C" {
+void foo() {
+    printf("Hello\n");
+}
+
+int bar = 300;
+}
---
+++ projects/replex-3/main/src/main.cpp
@@ -0,0 +1,26 @@
+#include <hello.h>
+
+#include <iostream>
+
+int main() {
+    HelloModule::LoadLibrary();
+    HelloModule::Foo();
+    int bar = HelloModule::GetBar();
+    std::cout << "bar == " << bar << std::endl;
+
+    // Modify the source code and recompile the library.
+    std::string filename = "./hello/src/hello.cpp";
+    std::string command = std::string("sed -i ") +
+                          (bar == 200 ? "'s/200/300/'" : "'s/300/200/'") + " " +
+                          filename;
+    system(command.c_str());
+    command = std::string("sed -i ") +
+              (bar == 200 ? "'s/Hi/Hello/'" : "'s/Hello/Hi/'") + " " + filename;
+    system(command.c_str());
+    system("cmake --build build");
+
+    HelloModule::ReloadLibrary();
+    HelloModule::Foo();
+    std::cout << "bar == " << HelloModule::GetBar() << std::endl;
+    return 0;
+}
