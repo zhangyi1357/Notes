c16ad3ee9763f5cfcd095a73713d4cbfa885263c
zhangyi1357 <zhangyi_1357@sjtu.edu.cn>
2024-03-03 13:33:37 +0800

    threadsafe_queue small refinement

--- concurrency/threadsafe_queue.cpp
+++ concurrency/threadsafe_queue.cpp
@@ -19,7 +19,7 @@ public:
 
     void push(T new_value) {
         std::lock_guard lk(mut);
-        data_queue.push(new_value);
+        data_queue.push(std::move(new_value));
         data_cond.notify_one();
     }
 
